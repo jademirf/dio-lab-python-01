cbaeedcc9979e1de5464f80685fa99be30a1a0cf
Jademir de Moura <jademir@policiacivil.pe.gov.br>
2023-07-24 13:32:48 -0300

    dio lab challenge 01

---
+++ dio_bank.py
@@ -0,0 +1,56 @@
+menu = """ 
+
+[d] Depositar
+[s] Sacar
+[e] Extrato
+[q] Sair
+
+=> """
+
+saldo = 0
+limite = 500
+extrato = ""
+numero_saques = 0
+LIMITE_SAQUES = 3
+
+while True: 
+
+    opcao = input(menu)
+
+    if opcao == "d":
+        valor = float(input("Informe o valor do depósito: \n"))
+
+        if valor > 0: 
+            saldo += valor
+            extrato += f"Depósito: R$ {valor:.2f}\n"
+
+        else:
+            print("Não foi possível concluir a operação, valor informado não é válido.")
+    elif opcao == "s":
+        valor = float(input("informe o valor do saque: "))
+
+        print("\n---------------------------------------------")
+
+        if valor > saldo:
+            print("Você não possui saldo suficiente para o valor solicitado.")
+        elif valor > limite: 
+            print("Valor do saque ultrapassa o valor limite diário.")
+        elif numero_saques >= LIMITE_SAQUES:
+            print("Você já utilizou o limite diário de saques.")
+        elif valor > 0:
+            saldo -= valor
+            extrato += f"Saque: R$ {valor:.2f}\n"
+            numero_saques += 1
+        else:
+            print("Operação falhou, valor inválido")
+
+    elif opcao == "e":
+        print("\n=================== EXTRATO =================")
+        print("Não foram realizadas movimentações." if not extrato else extrato)
+        print("---------------------------------------------")
+        print(f"Saldo: R$ {saldo:.2f}")
+        print("=============================================")
+    elif opcao == "q":
+        break
+    else:
+        print("Opção inválida, por favor escolha uma das opções disponíveis no menu.")
